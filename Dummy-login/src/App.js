2d24d4d3d2a614ca3209525d9fcf979c886089a3
AmanLohani <71622444+AmanLohani@users.noreply.github.com>
2023-05-26 20:13:43 +0530

    Dummy-login add

---
+++ Dummy-login/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
+import { useContext } from "react";
+
+function App() {
+const Ctx = useContext(AuthContext);
+
+  return (
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!Ctx.isLoggedIn && <Login />} 
+        {Ctx.isLoggedIn && <Home  />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
