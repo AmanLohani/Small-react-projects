37ff816a0ea0d21282f3fb6cc80b71e0e4f0749f
AmanLohani <71622444+AmanLohani@users.noreply.github.com>
2023-05-26 20:58:23 +0530

    Quote writing app added

---
+++ Quote-writing-app/build/static/js/6.f4e6bdc5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[6],{54:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("p",{className:"centered",children:"Sorry! Page not found...."})}}}]);
+//# sourceMappingURL=6.f4e6bdc5.chunk.js.map
